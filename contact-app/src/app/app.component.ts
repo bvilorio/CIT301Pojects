9f8e998a5d120d557a0f8c60aac7035280395661
Brayan Vilorio <vil13003@byui.edu>
2017-01-26 09:19:07 -0700

    1/26/2017 contac-app deleted

--- contact-app/src/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ca-root',
-  templateUrl: './app.component.html'
-})
-export class AppComponent {
-
-}
--- contact-app/src/app/contacts/contact.ts
+++
@@ -1,5 +0,0 @@
-export class Contact {
-  constructor(public name, public description, public email, public phone, public imagePath, public group){
-
-  }
-}
